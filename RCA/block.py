4039fe12a4d9167e5c5da706a45bca591fdc9759
Mark Redd <redddog@gmail.com>
2018-01-03 21:48:19 -0700

    Making changes and starting a 2d top down adventure version of the game!

---
+++ RCA/block.py
@@ -0,0 +1,10 @@
+
+class Block(object):
+    """
+    Parent Interface for all non-moving Display surfaces
+    reference:
+    http://inventwithpython.com/pygame/chapter2.html
+    
+    """
+    pass
+    
