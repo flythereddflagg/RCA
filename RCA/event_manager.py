75ba2711cf7dcb699e15e8ba59935a05ea515e14
Mark Redd <redddog@gmail.com>
2018-02-09 11:58:50 -0700

    piecing together everything

--- RCA/event_manager.py
+++ RCA/event_manager.py
@@ -1,7 +1,31 @@
 # event_manager.py
 import pygame as pg
+from constants import *
 
 
 class EventManager():
-    pass
+
+    def __init__(self, engine=None):
+        self.engine = engine
+
+    def capture_events(self):
+        for event in pg.event.get():
+            if event.type==pg.QUIT:
+                self.running = False
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.plr.move(5,W)
+        if keys[pg.K_RIGHT]:
+            self.plr.move(5,E)
+        if keys[pg.K_UP]:
+            self.plr.move(5,N)
+        if keys[pg.K_DOWN]:
+            self.plr.move(5,S)
+        if not (keys[pg.K_LEFT] or\
+                keys[pg.K_RIGHT] or\
+                keys[pg.K_UP] or\
+                keys[pg.K_DOWN]):
+            self.plr.stand()
+        if keys[pg.K_BACKSPACE]:
+            self.running = False
 
