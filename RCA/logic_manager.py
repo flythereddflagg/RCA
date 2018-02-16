d0de57685acef24e407d02a4ab05b65ad28c1fb8
Mark Redd <redddog@gmail.com>
2018-02-16 09:07:12 -0700

    Minor changes refactoring

--- RCA/logic_manager.py
+++ RCA/logic_manager.py
@@ -23,34 +23,34 @@ class LogicManager():
         if self.player.rect.x > self.wslack:
             self.player.rect.x -= self.spd_plr
         else:
-            self.mv_background(self.spd_plr, W)
+            self.mv_cam(self.spd_plr, W)
         self.player.walk_animate(W)
         
     def right(self):
         if self.player.rect.x < self.eslack:
             self.player.rect.x += self.spd_plr
         else:
-            self.mv_background(self.spd_plr, E)
+            self.mv_cam(self.spd_plr, E)
         self.player.walk_animate(E)
         
     def up(self):
         if self.player.rect.y > self.nslack:
             self.player.rect.y -= self.spd_plr
         else:
-            self.mv_background(self.spd_plr, N)
+            self.mv_cam(self.spd_plr, N)
         self.player.walk_animate(N)
 
     def down(self):
         if self.player.rect.y < self.sslack:
             self.player.rect.y += self.spd_plr
         else:
-            self.mv_background(self.spd_plr, S)
+            self.mv_cam(self.spd_plr, S)
         self.player.walk_animate(S)
     
     def no_key(self):
         self.player.stand()
     
-    def mv_background(self,pixels, dr=None):
+    def mv_cam(self,pixels, dr=None):
         if dr == None: dr = self.player.direction
         for i in self.eng.background.sprites():
             if   dr == N:
@@ -63,21 +63,21 @@ class LogicManager():
                 i.rect.x += pixels
     
     def cam_correct(self):
-        if self.rect.x < CENTERX - CAMERASLACK or\
-                self.rect.x > CENTERX + CAMERASLACK or\
-                self.rect.y < CENTERY - CAMERASLACK or\
-                self.rect.y > CENTERY + CAMERASLACK:
-            if self.rect.y > CENTERY + CAMERASLACK:
-                self.rect.y -= 5
-                self.mv_background(5,S)
-            if self.rect.y < CENTERY - CAMERASLACK:
-                self.rect.y += 5
-                self.mv_background(5,N)
+        if self.player.rect.x < self.wslack or\
+                self.player.rect.x > self.eslack or\
+                self.player.rect.y < self.nslack or\
+                self.player.rect.y > self.sslack:
+            if self.player.rect.y > self.sslack:
+                self.player.rect.y -= self.spd_plr
+                self.mv_cam(self.spd_plr,S)
+            if self.player.rect.y < self.nslack:
+                self.player.rect.y += self.spd_plr
+                self.mv_cam(self.spd_plr,N)
             
-            if self.rect.x > CENTERX + CAMERASLACK:
-                self.rect.x -= 5
-                self.mv_background(5,E)
-            if self.rect.x < CENTERX - CAMERASLACK:
-                self.rect.x += 5
-                self.mv_background(5,W)
+            if self.player.rect.x > self.eslack:
+                self.player.rect.x -= self.spd_plr
+                self.mv_cam(self.spd_plr,E)
+            if self.player.rect.x < self.wslack:
+                self.player.rect.x += self.spd_plr
+                self.mv_cam(self.spd_plr,W)
 
