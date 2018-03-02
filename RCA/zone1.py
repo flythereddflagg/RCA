49fb441d02a2e1431f339ef3125252fa6d3dd4bd
Mark Redd <redddogjr@gmail.com>
2018-03-01 19:45:59 -0700

    added map editing funcitonality but having trouble with things. See error

--- RCA/zone1.py
+++ RCA/zone1.py
@@ -1,29 +1,33 @@
 # zone1.py
 from constants import *
 from block import Block
+import csv
 
 class Zone1():
     def __init__(self, eng):
         self.eng = eng
         self.background = self.eng.bkgnd
-        bgx0 = self.background.rect.x
-        bgy0 = self.background.rect.y
         background_path = "./sprites/backgrounds/zone1.png"
-        
-        self.blocks = [
-            # xpos, ypos, path
-            Block(
-                bgx0 + 510 + 60*i, 
-                bgy0 + 1620,
-                "./sprites/blocks/rock_wall.png")\
-            for i in range(10)
-            ]
-            self.eng.blocks.add(self.blocks)
-            self.eng.all_sprites.add(self.blocks)
-        
+        self.update()
     
     def update(self):
-        #self.blocks = 
-        pass
+        self.blocks = []
+        self.bgx0 = self.background.rect.x
+        self.bgy0 = self.background.rect.y
+        for sprt in self.eng.blocks.sprites():
+            sprt.kill()
+        with open('zone1.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                self.blocks.append(
+                    # xpos, ypos, path
+                    Block(
+                        row[0],
+                        self.bgx0 + int(row[1]), 
+                        self.bgy0 + int(row[2]),
+                        row[3],
+                        row[4]))
+        self.eng.blocks.add(self.blocks)
+        self.eng.all_sprites.add(self.blocks)
         
         
