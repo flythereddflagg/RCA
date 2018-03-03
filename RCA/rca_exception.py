8a3f77c767fb1b38663edfe781b6b5f1cf2ab80a
Mark Redd <redddogjr@gmail.com>
2018-03-02 19:19:03 -0700

    Fixed editing problem. Now need to make an efficient way of making enviroments and levels etc. Like a level editor.

---
+++ RCA/rca_exception.py
@@ -0,0 +1,2 @@
+# file for RCA exception class to report errors in the RCA config and other
+# errors specific to RCA
--- RCA/zone1.py
+++ RCA/zone1.py
@@ -1,7 +1,6 @@
 # zone1.py
 from constants import *
 from block import Block
-import csv
 
 class Zone1():
     def __init__(self, eng):
@@ -16,17 +15,20 @@ class Zone1():
         self.bgy0 = self.background.rect.y
         for sprt in self.eng.blocks.sprites():
             sprt.kill()
-        with open('zone1.csv', 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
+        with open('zone1.csv', 'r') as f:
+            for line in f:
+                if line[0] == '#': continue
+                row = line.split(',')
+                if not(len(row) == 5 or len(row) == 0):
+                    raise ("Invalid in-game configuration file syntax")
                 self.blocks.append(
                     # xpos, ypos, path
                     Block(
                         row[0],
                         self.bgx0 + int(row[1]), 
                         self.bgy0 + int(row[2]),
-                        row[3],
-                        row[4]))
+                        int(row[3]),
+                        int(row[4])))
         self.eng.blocks.add(self.blocks)
         self.eng.all_sprites.add(self.blocks)
         
