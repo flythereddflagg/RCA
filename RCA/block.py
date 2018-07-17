1211a0bc7dc0c37a0122fa2a5d0af646f0be0231
Mark Redd <redddogjr@gmail.com>
2018-07-16 19:34:17 -0600

    Minor changes

--- RCA/block.py
+++ RCA/block.py
@@ -1,30 +1,30 @@
 from constants import *
 from rca_sprite import SpriteRCA
 
+# see pygame.transform.rotozoom() ?
 
 class Block(SpriteRCA):
     def __init__(self, 
-            img_path,
-            xpos, 
-            ypos,
-            scale = 1,
-            rotate = 0):
+                image_path,
+                xposition, 
+                xposition,
+                scale = 1,
+                rotate = 0):
         
         super().__init__()
-        self.image = pg.image.load(img_path).convert_alpha()
-        # see pygame.transform.rotozoom() ?
+        self.image = pg.image.load(image_path).convert_alpha()
         if scale != 1:
             width, height = self.image.get_size()
             width *= scale
             height *= scale
             self.image = pg.transform.scale(
-                self.image, 
-                (int(width), int(height)))
+                        self.image,
+                        (int(width), int(height)))
         if rotate != 0:
             self.image = pg.transform.rotate(self.image, rotate)
         self.rect = self.image.get_rect()
-        self.rect.x = xpos
-        self.rect.y = ypos
+        self.rect.x = xposition
+        self.rect.y = xposition
         self.mask = pg.mask.from_surface(self.image)
 
         
