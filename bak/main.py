dec567236ce89c71bfce632fd0fe14a7f6d14803
Mark Redd <redddogjr@gmail.com>
2023-11-04 19:32:19 -0700

    starting from the ground up

--- bak/main.py
+++
@@ -1,6 +0,0 @@
-from rca import init_game, engine
-
-DATA_PATH = "./data"
-
-game_data = init_game(DATA_PATH)
-engine(game_data)
--- bak/rca_exception.py
+++
@@ -1,8 +0,0 @@
-# file for RCA exception class to report errors in the RCA config and other
-# errors specific to RCA
-
-
-
-class RCAException(Exception):
-    def __init__(self, message):
-        self.message = message
