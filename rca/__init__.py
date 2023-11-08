59f1c2923770951c2a6743a17f6526c76ac8c2c0
Mark Redd <redddogjr@gmail.com>
2023-11-07 20:56:11 -0700

    finalized key bindings for keyboard. This will be the main test vehicle for now.

--- rca/__init__.py
+++ rca/__init__.py
@@ -14,6 +14,7 @@ def init_game(data_path):
     pg.init()
     controllers = []
 
+    # detect and load controllers
     for i in range(0, pg.joystick.get_count()):
         controllers.append(pg.joystick.Joystick(i))
         controllers[-1].init()
@@ -30,7 +31,11 @@ def init_game(data_path):
     game.clock = pg.time.Clock()
     game.screen = pg.display.set_mode(game.SCREENSIZE)
     game.controllers = controllers
+    game.INV_KEY_BINDINGS = {
+        item:key for key, item in game.KEY_BINDINGS.items()
+    }
 
+    print(game.KEY_BINDINGS)
 
     return game
 
@@ -41,6 +46,7 @@ def run_game(game):
 
     while game.running:
         game_input = get_input(game)
+        if game_input: print(game_input)
         game.logic(game_input)
         draw_frame(game)
         
@@ -57,17 +63,26 @@ def get_input(game):
     if pg.QUIT in [event.type for event in events]: return ["QUIT"]    
 
     # get general events
-    for event in events:
-        print(event.type, event)
+    # for event in events:
+    #     print(event.type, event)
 
-    # get keyboard keys
-    game_input = [
+    # get pressed keyboard keys
+    # TODO make the input more sophisticated
+    valid_keys = game.INV_KEY_BINDINGS.keys()
+    pressed_keys = [
         i 
         for i, pressed in enumerate(pg.key.get_pressed()) 
-        if pressed
+        if (pressed and i in valid_keys)
     ]
-    if game_input: print(game_input)
+    # if pressed_keys: print(pressed_keys)
 
+    if game.KEY_BINDINGS['FORCE_QUIT'] in pressed_keys: return ['QUIT']
+
+    game_input = [
+        game.INV_KEY_BINDINGS[pressed_key]
+        for pressed_key in pressed_keys
+    ]
+    
     return game_input
 
 
