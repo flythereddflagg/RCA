8863ea10389765780d4be4d2b8c2331ce6ea9b94
Mark Redd <redddogjr@gmail.com>
2023-11-20 18:40:27 -0700

    added CX FREEZE as build system. To be used to do the final compile

---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys
+from cx_Freeze import setup, Executable
+
+game_description = """Red Castle Avenger - The Game"""
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {
+    'packages'      : [], 
+    'excludes'      : ['cx_freeze'],
+    'include_files' : [
+        ("assets", "assets")
+    ]
+}
+
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('./main.py', base=base, target_name = 'RCA')
+]
+
+setup_options = {
+    "name"           : 'RCA',
+    "version"        : '0.1.0',
+    "description"    : game_description,
+    "options"        : {'build_exe': build_options},
+    "executables"    : executables
+}
+
+
+setup(**setup_options)
+# to build:
+# conda init powershell
+# conda activate pygame_compile 
+# python setup.py build
