57e9b6fa4cac30e49c8d3c2c4809ba8ff33dca35
Mark Redd <redddogjr@gmail.com>
2023-12-04 22:17:55 -0700

    I got the animations to stop but now they are all stopping after one cycle. Need to figure out how to get just the animations that do not repeat to do stop after one cycle.

--- src/player.py
+++ src/player.py
@@ -2,153 +2,15 @@ import pygame as pg
 from .sprite import Character, Decal
 from .compass import Compass
 
-DEFAULT_ANIMATION = 'stand'
-NULL_PATH = "./assets/dummy/null.png"
 
 class Player(Character):
     def __init__(self, **options):
         super().__init__(**options)
-        self.todo_list = []
-        self.last_animate = None
-        self.key_frame_group = None
-        self.animate_data = None
-        self.last_direction = None
-        self.key_frame = None
-        self.animation_availible = False
-        self.animation_active = False
-        self.frame_time = 1
-        self.last_frame_time = 0
-        self.direction = Compass.DOWN
-        self.last_direction = self.direction
         # TODO: redo speeds in terms of subpixels so this can scale
         self.PLAYERSPEED = 300 # pixels per second
         self.dist_per_frame = self.PLAYERSPEED // self.game.FPS
-        # init animations
-        self.animation_availible = True
-        self.key_frame_groups = {}
-        self.animation = {}
-        self.default_image = self.image
-        self.alt_image = Decal(**{
-            "game": self.game,
-            "asset_path": NULL_PATH,
-            "startx":0,
-            "starty":0,
-        })
-        self.game.groups['player'].add(self.alt_image)
+        self.keys_held = {key:False for key in self.game.KEY_BIND.keys()}
 
-        for animation in self.options['animations']:
-            # load in each animation for a character 
-            # defined in their yaml data
-            animation_image = pg.image.load(
-                animation['asset_path']
-            ).convert_alpha()
-            new_size = [
-                dim * self.scale 
-                for dim in animation_image.get_rect().size
-            ]
-            animation_image = pg.transform.scale(animation_image, new_size)
-            # get the size of each animation frame
-            kf_sizex, kf_sizey = [
-                i * self.scale for i in animation['key_frame_size']
-            ]
-            # calclulate the number of frames in the image
-            n_keyframesx, n_keyframesy = [
-                bg // kf for bg, kf in zip(
-                    animation_image.get_rect().size, [kf_sizex, kf_sizey]
-                )
-            ]
-            # construct a frame group (matrix of sub-images)
-            key_frame_group = [[
-                    animation_image.subsurface(
-                        [kf_sizex * i, kf_sizey * j, kf_sizex, kf_sizey]
-                    ) for i in range(n_keyframesx)
-                ] for j in range(n_keyframesy)
-            ]
-            if len(key_frame_group) == 1:
-                # single frame edge case: give it 4 frames
-                key_frame_group = [
-                    key_frame_group[0] for _ in Compass.indicies
-                ]
-            self.animation[animation['id']] = animation
-            self.animation[animation['id']]['key_frames'] = key_frame_group
-             
-        self.animate_data = self.animation[DEFAULT_ANIMATION]
-        self.null_image = self.animation['null']['key_frames'][0][0]
-
-
-    def update(self):
-        for action in self.todo_list:
-            self.apply_action(action)
-
-        if not self.todo_list and not self.animation_active:
-            self.animate_data = self.animation[DEFAULT_ANIMATION]
-
-        # animate if applicable
-        if self.animation_availible:
-            self.animate()
-
-        # reset the todo_list
-        self.todo_list = []
-
-
-    def add_todo(self, action):
-        self.todo_list.append(action)
-
-
-    def set_key_frame(self):
-        self.key_frame = self.gen_from_list(self.key_frames, 
-            repeat=self.animate_data['repeat']
-        )
-        self.key_frame_time = self.gen_from_list(self.key_frame_times)
-        self.set_image(next(self.key_frame, None))
-        self.frame_time = next(self.key_frame_time)
-
-
-    def set_image(self, image):
-        if self.image == self.null_image:
-            self.alt_image.image = self.null_image
-        self.image = image
-        if image is None: return
-        if self.rect.size == image.get_rect().size: return
-        # set image to blank
-        # then just paste the new image on top of it
-        self.image = self.null_image
-        self.alt_image.image = image
-        self.alt_image.rect = self.alt_image.image.get_rect()
-        self.alt_image.rect.center = self.rect.center
-
-
-    def animate(self):
-        set_key_frame = False
-        # update animation if changed
-        if self.animate_data['id'] != self.last_animate:
-            set_key_frame = True
-            self.last_animate = self.animate_data['id']
-            self.key_frames = self.animate_data["key_frames"][self.direction]
-            self.key_frame_times = self.animate_data['key_frame_times']
-
-        # update direction if it has changed
-        if self.direction != self.last_direction:
-            set_key_frame = True
-            self.last_direction = self.direction
-            self.key_frames = self.animate_data["key_frames"][self.direction]
-        
-        if set_key_frame: self.set_key_frame()
-
-        cur_time = pg.time.get_ticks()
-        for _ in range((cur_time -  self.last_frame_time) // self.frame_time):
-            self.set_image(next(self.key_frame, None))
-            self.frame_time = next(self.key_frame_time)
-            self.last_frame_time = cur_time
-        
-        if self.image is None: # animation is done
-            self.animation_active = False
-            last_animate = self.last_animate
-            self.last_animate = self.animate_data['id']
-            self.animate_data = self.animation[last_animate]
-            self.set_key_frame()
-
-    
 
     def apply_action(self, action):
         if self.animation_active: return
@@ -160,17 +22,23 @@ class Player(Character):
             self.animate_data = self.animation['walk']
             
         elif action == "BUTTON_1":
+            self.keys_held[action] = True
             self.animation_active = True
             self.animate_data = self.animation['sword swing']
             # TODO: make it so that the action happens only once until you let go of the button.
-
 
         else:
             print(action + "! (no response)")
             return
 
-    def gen_from_list(self, item_list, repeat=True):
-        while True:
-            for item in item_list:
-                yield item
-            if not repeat: break
+    def update(self):
+        todo_list_bak = self.todo_list.copy()
+        self.todo_list = [
+            todo for todo in self.todo_list
+            if not self.keys_held[todo]
+        ]
+        super().update()
+        self.keys_held = {key:False for key in self.game.KEY_BIND.keys()}
+        for todo in todo_list_bak:
+            self.keys_held[todo] = True
+
--- src/sprite.py
+++ src/sprite.py
@@ -4,8 +4,12 @@ This module contains all the basic sprite classes in the game.
 import yaml
 import pygame as pg
 
-from.dict_obj import DictObj
+from .dict_obj import DictObj
+from .compass import Compass
 
+
+DEFAULT_ANIMATION = 'stand'
+NULL_PATH = "./assets/dummy/null.png"
 
 class Decal(pg.sprite.Sprite):
     """
@@ -48,10 +52,6 @@ class Decal(pg.sprite.Sprite):
         new_size = [dim * scale for dim in self.rect.size]
         self.image = pg.transform.scale(self.image, new_size)
         self.rect = self.image.get_rect()
-        
-    
-    def update(self):
-        pass
 
 
 class Block(Decal):
@@ -82,6 +82,80 @@ class Character(Block):
     Non-solid sprite that triggers interaction and moves 
     independently of the camera. Also can be animated.
     """
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.todo_list = []
+        self.last_animate = None
+        self.key_frame_group = None
+        self.animate_data = None
+        self.last_direction = None
+        self.key_frame = None
+        self.animation_availible = False
+        self.animation_active = False
+        self.frame_time = 1
+        self.last_frame_time = 0
+        self.direction = Compass.DOWN
+        self.last_direction = self.direction
+        
+        # init animations
+        self.animation_availible = True
+        self.key_frame_groups = {}
+        self.animation = {}
+        self.default_image = self.image
+        self.alt_image = Decal(**{
+            "game": self.game,
+            "asset_path": NULL_PATH,
+            "startx":0,
+            "starty":0,
+        })
+        self.game.groups['player'].add(self.alt_image)
+        for animation in self.options['animations']:
+            # load in each animation for a character 
+            # defined in their yaml data
+            animation_image = pg.image.load(
+                animation['asset_path']
+            ).convert_alpha()
+            new_size = [
+                dim * self.scale 
+                for dim in animation_image.get_rect().size
+            ]
+            animation_image = pg.transform.scale(animation_image, new_size)
+            # get the size of each animation frame
+            kf_sizex, kf_sizey = [
+                i * self.scale for i in animation['key_frame_size']
+            ]
+            # calclulate the number of frames in the image
+            n_keyframesx, n_keyframesy = [
+                bg // kf for bg, kf in zip(
+                    animation_image.get_rect().size, [kf_sizex, kf_sizey]
+                )
+            ]
+            # construct a frame group (matrix of sub-images)
+            key_frame_group = [[
+                    animation_image.subsurface(
+                        [kf_sizex * i, kf_sizey * j, kf_sizex, kf_sizey]
+                    ) for i in range(n_keyframesx)
+                ] for j in range(n_keyframesy)
+            ]
+            if len(key_frame_group) == 1:
+                # single frame edge case: give it 4 frames
+                key_frame_group = [
+                    key_frame_group[0] for _ in Compass.indicies
+                ]
+            self.animation[animation['id']] = animation
+            self.animation[animation['id']]['key_frames'] = key_frame_group
+             
+        self.animate_data = self.animation[DEFAULT_ANIMATION]
+        self.null_image = self.animation['null']['key_frames'][0][0]
+
+    
+    def gen_from_list(self, item_list, repeat=True):
+        while True:
+            for item in item_list:
+                yield item
+            if not repeat: break
+
+
     def move(self, direction, distance):
         xunit, yunit = direction
         addx, addy = distance * xunit, distance * yunit
@@ -95,6 +169,85 @@ class Character(Block):
             False, pg.sprite.collide_mask
         ):
             self.rect.move_ip(-xunit, -yunit) # move back 1
+    
+    
+    def update(self):
+        for action in self.todo_list:
+            self.apply_action(action)
+
+        if not self.todo_list and not self.animation_active:
+            self.animate_data = self.animation[DEFAULT_ANIMATION]
+
+        # animate if applicable
+        if self.animation_availible:
+            self.animate()
+
+        # reset the todo_list
+        self.todo_list = []
+
+
+    def add_todo(self, action):
+        self.todo_list.append(action)
+
+
+    def set_key_frame(self):
+        self.key_frame = self.gen_from_list(
+            self.key_frames, repeat=self.animate_data['repeat']
+        )
+        self.key_frame_time = self.gen_from_list(self.key_frame_times)
+        self.set_image(next(self.key_frame, None))
+        self.frame_time = next(self.key_frame_time)
+
+
+    def set_image(self, image):
+        if self.image == self.null_image:
+            self.alt_image.image = self.null_image
+        self.image = image
+        if image is None: return
+        if self.rect.size == image.get_rect().size: return
+        # set image to blank
+        # then just paste the new image on top of it
+        self.image = self.null_image
+        self.alt_image.image = image
+        self.alt_image.rect = self.alt_image.image.get_rect()
+        self.alt_image.rect.center = self.rect.center
+
+
+    def animate(self):
+        set_key_frame = False
+        # update animation if changed
+        if self.animate_data['id'] != self.last_animate:
+            set_key_frame = True
+            self.last_animate = self.animate_data['id']
+            self.key_frames = self.animate_data["key_frames"][self.direction]
+            self.key_frame_times = self.animate_data['key_frame_times']
+
+        # update direction if it has changed
+        if self.direction != self.last_direction:
+            set_key_frame = True
+            self.last_direction = self.direction
+            self.key_frames = self.animate_data["key_frames"][self.direction]
+        
+        if set_key_frame: self.set_key_frame()
+
+        cur_time = pg.time.get_ticks()
+        for _ in range((cur_time -  self.last_frame_time) // self.frame_time):
+            self.set_image(next(self.key_frame, None))
+            self.frame_time = next(self.key_frame_time)
+            self.last_frame_time = cur_time
+        
+        if self.image is None: # animation is done
+            self.animation_active = False
+            last_animate = self.last_animate
+            self.last_animate = self.animate_data['id']
+            self.animate_data = self.animation[last_animate]
+            self.set_key_frame()
+    
+    
+    def apply_action(self, action):
+        raise NotImplementedError(
+            "apply_action MUST BE IMPLEMENTED FOR EACH CHILD"
+        )
 
 
 SPRITE_MAP = DictObj(**{
