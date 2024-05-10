97d4c143488e73ca28738e9b3751eca95db08c15
Mark Redd <redddogjr@gmail.com>
2024-05-09 22:21:25 -0700

    now begins the refactoring of this code. MAKE SURE YOU KEEP BACKUPS OF EVERYTHING

--- main.py
+++ main.py
@@ -36,3 +36,12 @@ if __name__ == "__main__":
 # TODO make scene transitions a la LTTP (polish)
 # TODO make a global zoom that scales the game to the size of the monitor
 # TODO implement Emma's character
+
+# TODO: Basic story and story beats from the first game
+# TODO: Items: [ ✓sword, ✓shovel, pickle, gate key, statue key]
+# TODO: Item mechanics completely fleshed out
+# TODO: Larry AND Holly are playable with these items but both play differently and have different exits at the end (e.g. Larry has less knockback from bludgeoning but is more susceptible to piercing while Holly is the opposite.) (exits are the Original exit to the village and the exit to the mountain or something TBD)
+# TODO: Enemies: [ wolf, ossifrage, goblin (club and arrows), and Rouken the dragon]
+# TODO: Boss fight must have cheese in the form of the chandelier
+# TODO: Basic parallax in the background or major foreground
+# TODO: A good amount of polish so that the game is "complete"
