6e33be04728475c41da75b5f85522899ccc3a512
Mark Redd <redddogjr@gmail.com>
2024-05-11 22:16:22 -0700

    refactor 1

--- main.py
+++ main.py
@@ -25,17 +25,13 @@ if __name__ == "__main__":
 
 # GENERAL TO-DO LIST
 # TODO rewrite UML for updated data structure SCHEME
-# TODO write down character and enemy ideas
-# TODO make at least three enemies (not including the dragon)
 # TODO make meaningful TESTS
 # TODO make a pause and save screen or whatever
 # TODO flesh out HUD
 # TODO make a startup screen
 # TODO make a load screen
-# TODO implement the RCA W1 story
 # TODO make scene transitions a la LTTP (polish)
 # TODO make a global zoom that scales the game to the size of the monitor
-# TODO implement Emma's character
 
 # TODO: Basic story and story beats from the first game
 # TODO: Items: [ ✓sword, ✓shovel, pickle, gate key, statue key]
