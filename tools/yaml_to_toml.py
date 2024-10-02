47d2dce5054f98c27f31bf7007c28d5db5d2be81
Mark Redd <redddogjr@gmail.com>
2024-10-01 20:56:27 -0700

    added more to the TOML stuff but it is slightly messed up.

--- tools/yaml_to_toml.py
+++ tools/yaml_to_toml.py
@@ -3,6 +3,29 @@ import yaml
 import sys
 
 
+def map_to_toml(key, item):
+    if isinstance(item, str):
+        return f"{key} = \"{item}\""
+    elif isinstance(item, bool):
+        return f"{key} = {str(item).lower()}"
+    elif isinstance(item, list):
+        return list_str_repr(item, key)
+    elif item is None:
+        return f"{key} = \"\""
+    else:
+        return f"{key} = {item}"    
+    
+
+def list_str_repr(a_list:list, master_key:str):
+    out_str = ""
+    if a_list and isinstance(a_list[0], dict):
+        for item in a_list:
+            out_str += f"\n[[{master_key}]]\n"
+            for key, val in item.items():
+                out_str += f"{key} = {map_to_toml(key, val)}\n"
+    else: return f"{master_key} = {str(a_list)}"
+    
+    return out_str
 
 for file in pathlib.Path(".").glob("./**/*.yaml"):
     subs = []
@@ -10,26 +33,18 @@ for file in pathlib.Path(".").glob("./**/*.yaml"):
     with open(file) as f:
         content = yaml.load(f.read(), Loader=yaml.Loader)
     with open(str(file)[:-5]+".toml", 'w') as f:
-        print("# converted from:", file, file=f)
+    # if True:
+    #     f = sys.stdout
+        print("#--------\n# converted from:", file, file=f)
         for key, item in content.items():
             if isinstance(item, dict):
                 subs.append([key, item])
             else:
-                if isinstance(item, str):
-                    item = f"\"{item}\""
-                elif isinstance(item, bool):
-                    item = str(item).lower()
-                elif isinstance(item, list):
-                    item = str(item).replace(':', ' =')
-                elif item is None:
-                    item = "\"\""
-                print(f"{key} = {item}", file=f)
+                print(map_to_toml(key, item), file=f)
         for key, item in subs:
             print(f"\n[{key}]", file=f)
             for subkey, subitem in item.items():
-                if isinstance(subitem, str):
-                    subitem = f"\"{subitem}\""
-                # print(f"{key}.{subkey} = {subitem}")
-                print(f"{subkey} = {subitem}", file=f)
+                sub_key_val = map_to_toml(subkey, subitem)
+                print(sub_key_val, file=f)
             print("", file=f)
 
