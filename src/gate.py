c7d6992d0240e48a4c6d5ab5fb39fa46aa9f059e
Mark Redd <redddogjr@gmail.com>
2025-01-18 17:02:42 -0700

    We got both gates set up and keys to go with them. Now, to have the shovel dig them up!

--- src/gate.py
+++ src/gate.py
@@ -5,6 +5,10 @@ from .decal import Decal
 collide_dist = 50**2 # pixels
 
 class Gate(Decal):
+    def __init__(self, key_id=None, **kwargs):
+        super().__init__(**kwargs)
+        self.key_id = key_id
+
     def update(self):
         player = self.scene.game.player
         dist_sqr = (
@@ -13,10 +17,10 @@ class Gate(Decal):
         ).length_squared()
         if (
             dist_sqr//self.scene.game.SCALE**2 < collide_dist and
-            player.inventory.possesed("gate key")
+            player.inventory.possesed(self.key_id)
         ):
             assert (
-                player.inventory.remove_item("gate key"), 
+                player.inventory.remove_item(self.key_id), 
                 "gate key was possesed but did not get removed properly"
             )
             self.kill()
