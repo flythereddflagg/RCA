4fc395a03ecb9386193e02fdf454f7e69538183f
Mark Redd <redddogjr@gmail.com>
2025-09-20 17:14:45 -0700

    OH YEAH WE GOT SOME MUSIC! Now to flesh out the rest so it feels cohesive.

--- src/music.py
+++ src/music.py
@@ -2,17 +2,61 @@ import pygame as pg
 
 from .node import Node
 
+# TODO fix slight glitch between transitions
+
 class Music(Node):
 
     def setup(self):
         self.filename = self.init.get("filename")
         assert self.filename, "No filename given"
-        self.sprite = None
+        self.measure_counter = 0.0
+        # total measures that have played per music.get_pos
+        self.last_update = 0.0 
+        self.measure_time = 1.846
+        seq_list = self.init.get("sequence")
+        if seq_list is not None:
+            self.sequence = iter(seq_list)
+            self.cur = next(self.sequence)
+            self.require_attr("measure_time")
+            self.measure_time = self.init.get("measure_time")
+        else:
+            self.sequence = None
+            self.cur = None
+            self.measure_time = None
+
+
         pg.mixer.music.load(self.filename)
         pg.mixer.music.play(-1)
+ 
     
     def update(self):
-        pass
+        if self.sequence is None: return
+        
+        ms_elapsed = pg.mixer.music.get_pos()
+        self.measure_counter = (
+            ms_elapsed
+            / 1000.0 
+            / self.measure_time
+        ) - self.last_update
+        # print(self.measure_counter)
+        if self.measure_counter >= self.cur[0]:
+            trigger, action, target = self.cur
+            next_ = next(self.sequence, None)
+            self.cur = self.cur if next_ is None else next_
+            self.last_update = (
+                ms_elapsed
+                / 1000.0 
+                / self.measure_time
+            )
+            # print(
+            #     trigger, action, target, 
+            #     self.measure_time, 
+            #     target * self.measure_time
+            # )
+            if action == "goto":
+                pg.mixer.music.rewind()
+                pg.mixer.music.set_pos(target * self.measure_time)
+
 
 
     def deconstruct(self):
