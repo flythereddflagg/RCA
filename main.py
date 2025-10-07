17d3d022e17a64b73c8c45d4581738edd5e40096
Mark Redd <redddogjr@gmail.com>
2025-10-06 21:55:33 -0700

    added a few minor TODOs

--- main.py
+++ main.py
@@ -75,4 +75,7 @@ TODO -1- make a proper game over screen
 TODO -1- make inventory have proper labeling too!
 TODO -4- make it possible to upload feedback from the menu
 TODO -4- animate the river/waterfall
+TODO -1- fix gate in background of forest. It was mistakenly added in bg
+TODO -3- fix player jitter when camera moves
+TODO -1- make glyph inactive buttons transparent
 """
