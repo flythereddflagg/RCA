a6037d3dba78865dbfda1f9eccab4358e0b53507
Mark Redd <redddogjr@gmail.com>
2025-11-15 22:01:18 -0700

    added dialogue with rosie. Now how does this work?

--- src/rosie.py
+++ src/rosie.py
@@ -11,24 +11,42 @@ class Rosie(Decal):
         super().setup()
         self.key_id = self.init.get("key_id")
         self.text_init = self.init.get("textbox")
+        self.hitmask = Decal(mask_path=self.init.get("image"))
         self.textbox = None
         self.talking = False
+        self.debounce_time = 0
+        self.debounce_length = 2000 # 2 seconds
 
     def update(self):
-        player_sprite = self.scene.get_player().sprite
-        if mask_collision(self.sprite, player_sprite) and not self.talking:
+
+        player_rect = self.scene.get_player().sprite.rect
+        if self.sprite.rect.colliderect(player_rect) and not self.talking:
             self.talking = True
             self.talk()
         if self.textbox:
-            if not self.textbox.scrolling:
+            if (
+                not self.textbox.scrolling
+                and self.debounce_time != -1
+                # and pg.time.get_ticks() - self.debounce_time 
+                # > self.debounce_length
+            ):
                 self.textbox.sprite.kill()
                 self.textbox = None
                 self.scene.paused = False
+                self.debounce_time = pg.time.get_ticks() 
             else:
                 self.textbox.update()
+                if self.debounce_time == -1: # debounce time not yet set
+                    self.debounce_time = pg.time.get_ticks() 
         else:
             if self.talking:
-                self.talking = False
+                if (
+                    pg.time.get_ticks() 
+                    - self.debounce_time 
+                    > self.debounce_length
+                ):
+                    self.talking = False
+                    self.debounce_time = -1
     
 
     def talk(self):
