f3e279167df3c690e538df6ff5cd5099ef824c3f
Mark Redd <redddogjr@gmail.com>
2026-01-10 21:53:31 -0700

    worth a shot for the TODO

--- src/selectmenu.py
+++ src/selectmenu.py
@@ -53,6 +53,7 @@ class SelectMenu(Node):
         
         elif select_pressed and self.active:
             self.close_menu()
+            # TODO -5- make it so slectpressed makes the menu dissapear regardless
         
         if self.active:
             if self.up_pressed():
