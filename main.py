0732706bcad15ad9e1043ff68ba274b38239e071
Mark Redd <redddogjr@gmail.com>
2026-01-13 22:35:21 -0700

    added TODO but got a lot more done!

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ TODO -4- make inventory have proper labeling too!
 TODO -4- make it possible to upload feedback from the menu
 TODO -4- animate the river/waterfall
 TODO -3- fix player jitter when camera moves
-
+TODO -4- refactor the menu system to be unified
 TODO -3- make meaningful TESTS
 TODO -3- make scene transitions a la LTTP (polish)
 TODO -4- Larry AND Holly are playable with these items but both play differently and have different exits at the end (e.g. Larry has less knockback from bludgeoning but is more susceptible to piercing while Holly is the opposite.) (exits are the Original exit to the village and the exit to the mountain or something TBD)
