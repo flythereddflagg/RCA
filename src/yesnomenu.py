8e375168c20b79aa86115385521bfcaad283017a
Mark Redd <redddogjr@gmail.com>
2026-02-26 06:53:08 -0700

    added more of the dialogue and now we need to figure out how the yesno menu interacts with everything

---
+++ src/yesnomenu.py
@@ -0,0 +1,66 @@
+import pygame as pg
+import yaml
+
+from .decal import Decal
+from .tools import vec
+from .menuinterface import MenuInterface
+
+
+MENU_TEXT = """
+Yes
+No
+""".strip()
+INDICATOR_X_OFFSET = 0
+TEXT_PADDING = 11
+
+YES_NO_MENU_INIT = """
+id: yesno
+type: YesNoMenu
+font_size: 22
+text_padding: 3
+bg_color: [0,0,0,0]
+outline: true
+children:
+- id: indicator
+  type: Decal
+  image: ./assets/block/text_select.png
+"""
+
+YES_NO_MENU_DICT = yaml.load(YES_NO_MENU_INIT, Loader=yaml.Loader)
+
+class YesNoMenu(MenuInterface):
+    def setup(self):
+        super().setup()
+        self.update_text_display()
+        self.textbox.sprite.rect.center = self.scene.game.get_center()
+        self.n_choices = len(MENU_TEXT.split("\n"))
+        self.active = False
+        self.selected = 0
+        self.bindings = [
+            self.do_yes,
+            self.do_no
+        ]
+
+    def update(self):
+        if not self.active:
+            self.kill()
+            return
+        if self.up_pressed():
+            self.go_up()
+        elif self.down_pressed():
+            self.go_down()
+        elif self.confirm_pressed():
+            self.bindings[self.selected]()
+
+
+    def do_yes(self):
+        print("Doing YES")
+
+
+    def do_no(self):
+        print("Doing NO")
+
+
+
+
+
