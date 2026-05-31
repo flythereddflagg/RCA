36535fc03a868233b7a59ff667aeb292447179cf
Mark Redd <redddogjr@gmail.com>
2026-05-30 21:40:37 -0700

    added format file

--- csrc/main.c
+++ csrc/main.c
@@ -4,25 +4,18 @@
 #define ASPECT_RATIO (16.0 / 9.0)
 #define RESOLUTION 360
 
-
-int main(void)
-{
-    InitWindow(
-        (int) RESOLUTION * ASPECT_RATIO, 
-        RESOLUTION, 
-        "Raylib - Red Castle Avenger"
-    );
+int main(void) {
+    InitWindow((int)RESOLUTION * ASPECT_RATIO, RESOLUTION,
+               "Raylib - Red Castle Avenger");
     SetTargetFPS(90);
-    Image image = LoadImage(
-        "./assets/scene/red_castle_valley/red_castle_valley_bg.png"
-    );
+    Image image =
+        LoadImage("./assets/scene/red_castle_valley/red_castle_valley_bg.png");
     Texture2D background = LoadTextureFromImage(image);
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawTexture(background, 0, 0, WHITE);
+        ClearBackground(BLACK);
+        DrawTexture(background, 0, 0, WHITE);
         EndDrawing();
     }
     UnloadTexture(background);
