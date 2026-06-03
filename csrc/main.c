c87831103cbfe4a712c13ba1715fa11d5f769b6e
Mark Redd <redddogjr@gmail.com>
2026-06-02 20:17:52 -0700

    okay so we have an idea about how to load and unload data here. This is good. We are making progress.

--- csrc/main.c
+++ csrc/main.c
@@ -1,37 +1,65 @@
 #include "raylib.h"
+#include "dbg.h"
 
 #define INIT_PATH "./assets/init.yaml"
 #define ASPECT_RATIO (16.0 / 9.0)
 #define RESOLUTION 360
+#define MAX_SPRITES 256
 
 typedef struct {
     Texture2D image;
     Vector2 position;
 } Sprite;
 
+typedef struct {
+    unsigned char len;
+    Sprite *arr;
+} SpriteArray;
+
+void add_sprite(SpriteArray *sprites, Sprite sprite){
+    if (sprites->len >= MAX_SPRITES)
+        return;
+    sprites->arr[sprites->len] = sprite;
+    sprites->len += 1;
+}
+
 int main(void) {
-    InitWindow((int)RESOLUTION * ASPECT_RATIO, RESOLUTION,
+    InitWindow((int)(RESOLUTION * ASPECT_RATIO), RESOLUTION,
                "Raylib - Red Castle Avenger");
     SetTargetFPS(90);
-    Sprite background = (Sprite){
+    Sprite mem[MAX_SPRITES];
+    SpriteArray sprites = 
+        (SpriteArray)
+        {
+            .len=0,
+            .arr=&(mem[0])
+    };
+    debug("sizeof everything texture %d image %d", sizeof(Vector2), sizeof(Image));
+    add_sprite(&sprites, (Sprite){
         .image=LoadTexture(
             "./assets/scene/red_castle_valley/red_castle_valley_bg.png"),
         .position=(Vector2){0, 0}
-    };
-    Sprite larry = (Sprite){
+    });
+    add_sprite(&sprites, (Sprite){
         .image=LoadTexture("assets/actor/larry/larry_base.png"),
         .position=(Vector2){300, 300}   
-    };
+    });
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawTexture(background.image, background.position.x, background.position.y, WHITE);
-        DrawTexture(larry.image, larry.position.x, larry.position.y, WHITE);
+        for (int i = 0; i < sprites.len; i++)
+            DrawTexture(
+                sprites.arr[i].image, 
+                sprites.arr[i].position.x, 
+                sprites.arr[i].position.y, 
+                WHITE
+            );
         EndDrawing();
     }
-    UnloadTexture(background.image);
-    UnloadTexture(larry.image);
+
+    for (int i = 0; i < sprites.len; i++)
+        UnloadTexture(sprites.arr[i].image);
     CloseWindow();
 
     return 0;
