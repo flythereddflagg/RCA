46b0c4ab6b628b9c8350f256a63db5ad1f33f44e
Mark Redd <redddogjr@gmail.com>
2026-06-03 21:38:24 -0700

    added a basic gamplay loop!

--- csrc/main.c
+++ csrc/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include "raylib.h"
 #include "dbg.h"
 
@@ -5,6 +6,8 @@
 #define ASPECT_RATIO (16.0 / 9.0)
 #define RESOLUTION 360
 #define MAX_SPRITES 256
+#define MAX_INPUTS 6
+#define SPEED 200
 
 typedef struct {
     Texture2D image;
@@ -16,6 +19,8 @@ typedef struct {
     Sprite *arr;
 } SpriteArray;
 
+typedef enum {NONE, UP, RIGHT, DOWN, LEFT} Direction;
+
 void add_sprite(SpriteArray *sprites, Sprite sprite){
     if (sprites->len >= MAX_SPRITES)
         return;
@@ -23,7 +28,57 @@ void add_sprite(SpriteArray *sprites, Sprite sprite){
     sprites->len += 1;
 }
 
+void input(Direction *inputs){
+    if (IsKeyDown(KEY_UP)) inputs[0] = UP;
+    if (IsKeyDown(KEY_DOWN)) inputs[1] = DOWN;
+    if (IsKeyDown(KEY_LEFT)) inputs[2] = LEFT;
+    if (IsKeyDown(KEY_RIGHT)) inputs[3] = RIGHT;
+}
+
+void logic(Direction *inputs, SpriteArray *sprites){
+    Sprite *larry = &(sprites->arr[1]);
+    int dist = (int) SPEED * GetFrameTime();
+    for (int i = 0; i < MAX_INPUTS; i++){
+        switch (inputs[i]){
+            case UP:
+                larry->position.y -= dist;
+                break;
+
+            case DOWN:
+                larry->position.y += dist;
+                break;
+
+            case RIGHT:
+                larry->position.x += dist;
+                break;
+
+            case LEFT:
+                larry->position.x -= dist;
+                break;
+            
+            default:
+                ;
+        }
+        inputs[i] = 0;
+    }
+}
+
+void draw(SpriteArray *sprites){
+    BeginDrawing();
+    ClearBackground(BLACK);
+    for (int i = 0; i < sprites->len; i++)
+        DrawTexture(
+            sprites->arr[i].image, 
+            sprites->arr[i].position.x, 
+            sprites->arr[i].position.y, 
+            WHITE
+        );
+    EndDrawing();
+}
+
+
 int main(void) {
+    // init
     InitWindow((int)(RESOLUTION * ASPECT_RATIO), RESOLUTION,
                "Raylib - Red Castle Avenger");
     SetTargetFPS(90);
@@ -34,7 +89,6 @@ int main(void) {
             .len=0,
             .arr=&(mem[0])
     };
-    debug("sizeof everything texture %d image %d", sizeof(Vector2), sizeof(Image));
     add_sprite(&sprites, (Sprite){
         .image=LoadTexture(
             "./assets/scene/red_castle_valley/red_castle_valley_bg.png"),
@@ -44,20 +98,21 @@ int main(void) {
         .image=LoadTexture("assets/actor/larry/larry_base.png"),
         .position=(Vector2){300, 300}   
     });
+    Direction input_array[MAX_INPUTS] = {0};
+    Direction *inputs = &(input_array[0]);
+    SetExitKey(KEY_BACKSPACE);
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        for (int i = 0; i < sprites.len; i++)
-            DrawTexture(
-                sprites.arr[i].image, 
-                sprites.arr[i].position.x, 
-                sprites.arr[i].position.y, 
-                WHITE
-            );
-        EndDrawing();
+    // mainloop
+    bool running = true;
+    while (running) {
+        if (WindowShouldClose())
+            running = false;
+        input(inputs);
+        logic(inputs, &sprites);
+        draw(&sprites);
+
     }
-
+    // cleanup
     for (int i = 0; i < sprites.len; i++)
         UnloadTexture(sprites.arr[i].image);
     CloseWindow();
