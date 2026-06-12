d023a66c1283e80d3f8a6431611a741bdb36b0b2
Mark Redd <redddogjr@gmail.com>
2026-06-11 19:25:28 -0700

    SET works now we need GET and DELETE

--- csrc/dict.c
+++ csrc/dict.c
@@ -2,8 +2,9 @@
 #include <string.h>
 #include "dbg.h"
 #define DICTKEYLENGTH 256
-#define DICTSIZE 255
+#define DICTSIZE 3
 #define HASHPRIME 31
+#define NO_NEXT -1
 
 typedef struct DictData* Dict;
 typedef union DictValData DictVal;
@@ -35,7 +36,7 @@ int Dict_hash(const DictKey key){
       hash += pow(key[i], i + 1) * HASHPRIME;
     }
     check(i > 0, "invalid hash key");
-    debug("hash %lld\n", hash);
+    debug("hash %lld", hash);
     return hash % DICTSIZE;
     
 error:
@@ -43,10 +44,32 @@ error:
 }
 
 int Dict_set(Dict self, DictKey key, DictType type, DictVal val){
-    int index = Dict_hash(key);
-    if ((*self->keys[index]) && strncmp(key, self->keys[index], DICTKEYLENGTH)){
-        
+    check(self, "invalid dict supplied");
+    check(key[0], "invalid key supplied");
+    // get the hash value
+    int hash_i = Dict_hash(key), index = 0, i;
+
+    // if the key at that index is not empty follow the linked list to the end
+    while (self->next[hash_i] != NO_NEXT)
+        hash_i = self->next[hash_i];
+    
+    index = hash_i;
+    // now we have the end of the current hash list
+    // so we increment by 1 until we either find an empty slot or the given key
+    for (i = 0; i < DICTSIZE; i++){
+        if (!self->keys[index] 
+            || !strncmp(key, self->keys[index], DICTKEYLENGTH)
+        ) break;
+        index++;
+        // wrap around
+        if (index >= DICTSIZE)
+            index -= DICTSIZE;
     }
+    check(i < DICTSIZE, "Dict is full");
+    // then if we needed to find another slot, we link to it in the previous one
+    if (hash_i != index)
+        self->next[hash_i] = index;
+
     check(index >= 0 && index < DICTSIZE, "invalid index detected");
     self->keys[index] = key;
     self->types[index] = type;
@@ -55,62 +78,74 @@ int Dict_set(Dict self, DictKey key, DictType type, DictVal val){
 error:
     return -1;
 }
-    
+
+int Dict_delete(Dict self, DictKey key){
+error:
+    return -1;
+}    
 
 DictVal Dict_get(Dict self, DictKey key, DictVal _default){
     return (DictVal) {.num=0};
 }
 
-void Dict_printrepr(Dict yaml){
+
+void Dict_printrepr(Dict dict){
     char *str = "";
     
     for (int i = 0; i < DICTSIZE; i++){
-        if (!yaml->keys[i]) continue;
-        printf("[%3d] %10s | ", i, yaml->keys[i]);
-        switch (yaml->types[i]){
+        if (!dict->keys[i]) continue;
+        printf("[%3d] %10s | ", i, dict->keys[i]);
+        switch (dict->types[i]){
         case NONE:
-            printf(" none %20s", "NONE");
+            printf(" none %20s", "null");
             break;
         case DICT:
         case ARR:
         case STR:
-            printf("  str %20s", yaml->vals[i].str);
+            printf("  str %20s", dict->vals[i].str);
             break;
         case INT:
-            printf("  int %20lld", yaml->vals[i].num);
+            printf("  int %20lld", dict->vals[i].num);
             break;
         case FLOAT:
-            printf("float %20f", yaml->vals[i].fnum);
+            printf("float %20f", dict->vals[i].fnum);
             break;
         case BOOL:
-            printf(" bool %20d", yaml->vals[i]._bool);
+            printf(" bool %20d", dict->vals[i]._bool);
             break;
         default:
             sentinel("invalid type")
             break;
         }
-        printf(" | %d\n",yaml->next[i]);
+        if (dict->next[i] == NO_NEXT)
+            printf(" | null\n");
+        else
+            printf(" | %d\n",dict->next[i]);
     }
 error:
     return;
 }
 
 int main() {
-    log_info("\n compile successful\n");
-    DictKey dkeys[DICTSIZE] = {""};
-    DictType dtypes[DICTSIZE] = {NONE};
+    log_info("compile successful");
+    DictKey dkeys[DICTSIZE] = {0};
+    DictType dtypes[DICTSIZE] = {0};
     DictVal dvals[DICTSIZE] = {0};
-    int dnext[DICTSIZE] = {-1};    
-    struct DictData dyaml = 
+    int dnext[DICTSIZE] = {0};
+    for (int i = 0; i < DICTSIZE; i++)
+        dnext[i] = NO_NEXT;
+    struct DictData ddict = 
         (struct DictData) {
             .keys = &dkeys[0],
             .types = &dtypes[0],
             .vals = &dvals[0],
             .next = &dnext[0]
     };
-    Dict yaml = &dyaml;
-    Dict_set(yaml, (DictKey)"pickle", INT, (DictVal){.num=25});
-    Dict_set(yaml, (DictKey)"cheese", STR, (DictVal){.str="23"});
-    Dict_printrepr(yaml);
+    Dict dict = &ddict;
+    Dict_set(dict, (DictKey)"pickle", INT, (DictVal){.num=25});
+    Dict_set(dict, (DictKey)"cheese", STR, (DictVal){.str="23"});
+    Dict_set(dict, (DictKey)"crackers", STR, (DictVal){.str="holy guacamole"});
+
+    Dict_printrepr(dict);
     return 0;
 }
