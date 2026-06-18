bea365554e0c279ba7293eb8f7f7e574758b1ecf
Mark Redd <redddogjr@gmail.com>
2026-06-17 21:45:33 -0700

    added a bit to tools but I do not know whether it works

--- csrc/tools.c
+++ csrc/tools.c
@@ -1,6 +1,7 @@
 #ifndef __TOOLS_C__
 #define __TOOLS_C__
 #include "cyaml.h"
+#include "dict.c"
 #include "dbg.h"
 #include "raylib.h"
 #include <stdlib.h>
@@ -18,5 +19,46 @@ Yaml Yaml_load(const char *yaml_path) {
 
 error:
     return NULL;
+}
+DictVal Yaml_get(DictType type, const char *name){
+    cyaml_as_int(
+        settings, 
+        cyaml_get(settings, cyaml_root(settings), "RESOLUTION"),
+        &resolution
+    
+    );
+    switch (dict->types[i]) {
+        case NONE:
+            printf(" none %20s", "null");
+            break;
+        case DICT:
+            Dict_printrepr(dict->vals[i].dict);
+            break;
+        case ARR:
+        case STR:
+            printf("  str %20s", dict->vals[i].str);
+            break;
+        case INT:
+            printf("  int %20lld", dict->vals[i].num);
+            break;
+        case FLOAT:
+            printf("float %20f", dict->vals[i].fnum);
+            break;
+        case BOOL:
+            printf(" bool %20d", dict->vals[i]._bool);
+            break;
+        case OBJ:
+            printf("  obj %20p", dict->vals[i].obj);
+            break;
+        default:
+            sentinel("invalid type") break;
+        }
+        if (dict->next[i] == NO_NEXT)
+            printf(" | null\n");
+        else
+            printf(" | %d\n", dict->next[i]);
+    }
+error:
+    return (DictVal) {.num=0};
 }
 #endif
