15fbad4659464ac332d27b7aea07dd43125ebe21
Mark Redd <redddogjr@gmail.com>
2026-06-27 22:14:40 -0700

    trying map tiling!

---
+++ assets/scene/world_map/isometric.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Iso" tilewidth="32" tileheight="32" tilecount="10" columns="10">
+ <image source="iso_tiles.png" width="320" height="32"/>
+</tileset>
