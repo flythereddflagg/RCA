2258b25df792be6a5f0052b83ccbc7e85b14857d
Mark Redd <redddogjr@gmail.com>
2026-08-25 21:18:15 -0700

    fixed the key-value problem! Now next TODO, the text is only parsed as text. How to parse as number or other?

--- csrc_test/test_yaml_dict.c
+++ csrc_test/test_yaml_dict.c
@@ -71,7 +71,11 @@ DynValue process_yaml_file(const char *filename) {
             key = true;
             break;
         case YAML_SCALAR_EVENT:
-
+            /*
+            check that the value is not 0 if it is return int = 0
+            try to parse as a int and a float. If int and float are the same value, parse as int, otherwise parse as float (use --strtol--)
+            if parsing error occurs then check for bool lower("true") and lower("false") only if all of these fail do you parse as string
+             */
             if (levels[top] == YAML_SEQ_STATE) {
                 ValArray_append(
                     stack[top]._arr_, STR,
